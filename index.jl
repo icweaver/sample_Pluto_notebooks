4b4a5fe9376b605110f308738ba1d06fb5fb6b77
icweaver <iweaver@cfa.harvard.edu>
2021-10-05 00:15:28 -0400

    added index

---
+++ index.jl
@@ -0,0 +1,13 @@
+### A Pluto.jl notebook ###
+# v0.16.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 906a22a4-cae0-11eb-009d-7d4c76978af6
+md"""
+[README](https://github.com/icweaver/sample_Pluto_notebooks#readme)
+"""
+
+# ╔═╡ Cell order:
+# ╟─906a22a4-cae0-11eb-009d-7d4c76978af6
